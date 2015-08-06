0926ac0ba0aa095224d14df95760bf0dbb4ac56c
Sebastian Real <seba.real.a@gmail.com>
2015-08-06 20:35:00 -0300

    correccion de diseño

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-//var imagemin = require('gulp-imagemin');
-//var pngquant = require('imagemin-pngquant');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 var tasks = {
     assets: function(){
@@ -21,9 +21,22 @@ var tasks = {
         .pipe(gulp.dest('./public/css'));
     },
 
+    images: function(){
+        //si karla tiene problemas, por favor comentar el contenido
+        // de esta funcion
+        var choices = {
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        };
+
+        gulp.src('./src/img/*')
+        .pipe(imagemin(choices))
+        .pipe(gulp.dest('./public/img/'));
+    },
+
     scripts: function(){
-        return gulp.src('./src/js/*.js')
-            .pipe(gulp.dest('./public/js/'));
+        return gulp.src('./src/js/*.js').pipe(gulp.dest('./public/js/'));
     }
 };
 
@@ -32,9 +45,9 @@ gulp.task('styl', tasks.styl);
 gulp.task('images', tasks.images);
 gulp.task('scripts', tasks.scripts);
 
-gulp.task('watch', ['assets', 'styl', 'scripts'], function(){
+gulp.task('watch', function(){
     gulp.watch(['./src/css/**/*.styl'], ['styl']);
     gulp.watch(['./src/js/*.js'], ['scripts']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['assets', 'styl', 'scripts', 'images' , 'watch']);
